00b79882bbd08257bd6687e6fe5606462181a5aa
Vindhya Tiwari <61542681+vindhay@users.noreply.github.com>
2020-12-26 16:21:27 +0530

    Add files via upload

---
+++ AssQ3.java
@@ -0,0 +1,22 @@
+class AssQ3
+{
+	public static void main(String args[])
+	{
+		int x=4;
+		int y=x*x+3*x-7;
+		System.out.println("A) y= "+y);
+		int y1=x++ + ++x;
+		System.out.println("B) y1= "+y1+" x= "+x);
+		int y2=5;
+		int z=x++ - --y2 - --x + x++;
+		System.out.println("C) x= "+x+" y2= "+y2+" z= "+z);
+		boolean a= true;
+		boolean b= true;
+		boolean c= a && b || !(a || b);
+		System.out.println("D)c= "+c);
+ 
+		
+		
+		
+	}
+}
